8744c77394f5b5d0c02f5f63115f0b9acfd7874e
Choco <94597336+ChocoMeow@users.noreply.github.com>
2023-10-12 12:03:04 +0800

    General clean code and added reward into quiz

--- views/quiz.py
+++ views/quiz.py
@@ -6,6 +6,12 @@ from iufi import (
     Question
 )
 
+LEVELS_BASE: dict[str, tuple[int, int, hex]] = {
+    "easy": (1, 3, 0x7CD74B),
+    "medium": (2, 2, 0xF9E853),
+    "hard": (3, 1, 0xD75C4B),
+}
+
 class AnswerModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(title="Enter your answer", *args, **kwargs)
@@ -33,8 +39,7 @@ class QuizView(discord.ui.View):
         self._ended_time: float = None
 
         self._answering_time: float = time.time()
-        self._is_answered: bool = False
-        self._is_timeout: bool = False
+        self._timeout: float = None
         self._results: list[bool] = [None for _ in range(len(self.questions))]
         self._average_time: list[float] = []
 
@@ -49,45 +54,73 @@ class QuizView(discord.ui.View):
             return await self.end_game()
         
         await asyncio.sleep(5)
-        self._is_timeout = False
-        self._is_answered = False
 
         self.current += 1
+        self._answering_time = time.time()
         await self.response.edit(embed=self.build_embed())
 
-        self._answering_time = time.time()
-        await self.counter(self.currect_question.average_time, self.current)
-    
-    async def counter(self, count: int, current_question: int) -> None:
-        await asyncio.sleep(count)
-        if current_question == self.current:
-            self._is_timeout = True
-
     async def end_game(self) -> None:
+        if self._ended_time:
+            return
+        
         self._ended_time = time.time()
-        embed = discord.Embed(title="Quiz Result", color=discord.Color.random())
-        embed.description = f"```{self.display_results()}```" \
-                            f"```{'Time Used:':<12} {func.convert_seconds(self.used_time)}\n" \
-                            f"{'Avg Time:':<12} {func.convert_seconds(sum(self._average_time) / len(self.questions))}```"
+        summary, total_points = self.cal_results()
 
+        user = await func.get_user(self.author.id)
+        state = user.get("game_state", {}).get("quiz_game", {
+            "points": 0,
+            "last_update": 0
+        })
+
+        sow, eow = func.get_week_unix_timestamps()
+        if not (sow <= state["last_update"] <= eow):
+            state["points"] = 0
+        
+        state["points"] += total_points
+        state["last_update"] = time.time()
+
+        embed = discord.Embed(title="Quiz Result", color=discord.Color.random())
+        embed.description = f"```{summary}```" \
+                            f"```{'🕔 Time Used:':<12} {func.convert_seconds(self.used_time)}\n" \
+                            f"{'🕘 Avg Time:':<12} {func.convert_seconds(sum(self._average_time) / len(self.questions))}\n" \
+                            f"{'🔥 Points:':<12} {state['points']} ({'+' if total_points >= 0 else '-'}{abs(total_points)})```"
+
+        await func.update_user(self.author.id, {"$set": {"game_state.quiz_game": state}})
         await self.response.edit(content="This quiz has expired.", embed=embed, view=None)
 
     def build_embed(self) -> discord.Embed:
         question: Question = self.currect_question
 
-        embed = discord.Embed(title=f"Question {self.current + 1} out of {len(self.questions)}", color=discord.Color.random())
+        embed = discord.Embed(title=f"Question {self.current + 1} out of {len(self.questions)}", color=LEVELS_BASE.get(question.level)[2])
         embed.description = f"**Answer Time: <t:{round(time.time() + question.average_time)}:R>**\n```{question.question}```"
         if question.attachment:
             embed.set_image(url=question.attachment)
 
         embed.set_footer(text=f"Correct: {question.correct_rate}% Wrong: {question.wrong_rate}%")
+
+        self._timeout = self._answering_time + question.average_time
         return embed
     
-    def display_results(self) -> str:
-        mapping = {True: "✅", False: "❌"}
-        string = ' '.join(mapping.get(result, "⬛") for result in self._results)
-        return string
+    def cal_results(self) -> tuple[str, float]:
+        summary = ""
+        total_points = 0.0
+        symbols = {True: "✅", False: "❌", None: "⬛"}
 
+        for question, result in zip(self.questions, self._results):
+            summary += symbols[result] + " "
+
+            if result is not None:
+                points = LEVELS_BASE.get(question.level)
+                total_points += points[0] if result else -points[1]
+            else:
+                total_points -= 0.5
+
+        return summary, total_points
+
+    @property
+    def total_time(self) -> float:
+        return sum([question.average_time for question in self.questions])
+    
     @property
     def used_time(self) -> float:
         return round(self._ended_time - self._start_time, 2)
@@ -98,7 +131,7 @@ class QuizView(discord.ui.View):
 
     @discord.ui.button(label="Answer", style=discord.ButtonStyle.green)
     async def answer(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
-        if self._is_answered:
+        if self._results[self.current] is not None:
             return await interaction.response.send_message("You are already answered! Please for the next question.", ephemeral=True, delete_after=5)
 
         question = self.currect_question
@@ -109,26 +142,19 @@ class QuizView(discord.ui.View):
         if self._ended_time:
             return 
 
-        if self._is_timeout:
-            message = await interaction.followup.send(f"You take too long to answer! Next question will be <t:{round(time.time() + 5)}:R>.", ephemeral=True)
+        used_time = time.time() - self._answering_time
+        self._average_time.append(used_time)
+        
+        if self._timeout < time.time():
+            msg = f"You take too long to answer! Next question will be <t:{round(time.time() + 5)}:R>."
         
         elif modal.answer:
-            used_time = time.time() - self._answering_time
-            self._average_time.append(used_time)
             question.update_average_time(used_time)
-
-            self._is_answered = True
             correct = question.check_answer(modal.answer)
-
-            if not correct:
-                question._wrong += 1
-                message: discord.Message = await interaction.followup.send(f"The correct response is `{self.currect_question.answers[0]}.`\nNext question will be <t:{round(time.time() + 5)}:R>.", ephemeral=True)
-            else:
-                question._correct += 1
-                message: discord.Message = await interaction.followup.send(f"You are correct! Next question will be <t:{round(time.time() + 5)}:R>.", ephemeral=True)
-
+            
+            msg = f"You are correct! Next question will be <t:{round(time.time() + 5)}:R>." if correct else f"The correct response is `{self.currect_question.answers[0]}.`\nNext question will be <t:{round(time.time() + 5)}:R>."
             self._results[self.current] = correct
 
+        message: discord.Message = await interaction.followup.send(msg, ephemeral=True)
         await message.delete(delay=5)
         return await self.next_question()
-
