ac4ebc92f8367cffa2cd70b69516e63286624ba2
Arieswaran <arieswaran@gmail.com>
2023-12-07 18:13:25 +0530

    gift drop view

---
+++ views/giftdrop.py
@@ -0,0 +1,72 @@
+import discord, time, asyncio
+import functions as func
+
+from iufi import CardPool, Card, POTIONS_BASE
+
+
+class GiftButton(discord.ui.Button):
+    def __init__(self, **kwargs):
+
+        self.gift_owner: discord.Member = None
+
+        super().__init__(
+            emoji="🎁",
+            style=discord.ButtonStyle.green,
+            **kwargs
+        )
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        self.view: GiftDropView
+
+        if owner_id := self.gift_owner.id:
+            if owner_id != interaction.user.id:
+                return await interaction.response.send_message(f"Santa's sleigh delivered this gift to <@{owner_id}>")
+            else:
+                return await interaction.response.send_message("Oh, ho, ho! You already received this gift! Naughty "
+                                                               "or nice, you can't claim it twice!")
+
+        self.gift_owner = interaction.user.id
+
+        await interaction.response.defer()
+
+        # add gift to user inventory
+        user = await func.get_user(interaction.user.id)
+
+        self.view.claimed_users.add(interaction.user)
+
+        self.disabled = True
+        self.style = discord.ButtonStyle.gray
+
+        await self.view.message.edit(view=self.view)
+        await interaction.followup.send(
+            f"{interaction.user.mention} is on Santa's nice list and has received a jolly gift!")
+
+
+class GiftDropView(discord.ui.View):
+    def __init__(self, author: discord.Member, *, timeout: float | None = None):
+        super().__init__(timeout=timeout)
+
+        self.claimed_users: set[discord.Member] = set()
+        self.message: discord.Message = None
+
+        for i in range(3):
+            self.add_item(GiftButton(custom_id=str(i)))
+
+    async def timeout_count(self):
+        await asyncio.sleep(120)
+        for child in self.children:
+            child.style = discord.ButtonStyle.gray
+            child.disabled = True
+        await self.message.edit(content="* Tinsel time's up! This Drop has melted away like a snowflake in the sun. ❄️",
+                                view=self)
+        self.stop()
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+
+        if interaction.user in self.claimed_users:
+            await interaction.response.send_message("Oh, ho, ho! Naughty "
+                                                    "or nice, you can't claim twice!",
+                                                    ephemeral=True)
+            return False
+
+        return True
