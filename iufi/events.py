3c1e549aadaa2b8eea201f1abd050bf5c09ed99e
Arieswaran <arieswaran@gmail.com>
2024-09-11 11:24:40 +0530

    milestone config update

--- iufi/events.py
+++ iufi/events.py
@@ -156,7 +156,7 @@ BUY_MESSAGE: list[str] = [
     "🚨 Alert! {0} bought the mystery card! Time to unveil the surprise!"
 ]
 
-MILESTONES = [2, 3, 4, 5]
+MILESTONES = [500, 2000, 5844, 10000]
 MILESTONE_ONE_REWARD = [["🎵", "candies", 50]]
 MILESTONE_TWO_REWARD = [["🌸", "roll.rare", 2]]
 MILESTONE_THREE_REWARD = [["🌸", "roll.rare", 1], ["💎", "roll.epic", 1]]
