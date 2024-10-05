1da7b4fc1e74f4654ce41decc726d8e001ac1797
Choco <94597336+ChocoMeow@users.noreply.github.com>
2024-10-05 08:46:06 +0800

    Fixed quest progress for card's upgrade

--- cogs/card.py
+++ cogs/card.py
@@ -504,9 +504,12 @@ class Card(commands.Cog):
         for card in converted_cards:
             iufi.CardPool.add_available_card(card)
         
-        query = func.update_quest_progress(await func.get_user(ctx.author.id), "UPGRADE_CARD", query={
-            "$pull": {"cards": {"$in": (card_ids := [card.id for card in converted_cards])}}
-        })
+        query = func.update_quest_progress(
+            await func.get_user(ctx.author.id),
+            "UPGRADE_CARD",
+            progress=len(converted_cards), 
+            query={"$pull": {"cards": {"$in": (card_ids := [card.id for card in converted_cards])}}}
+        )
         await func.update_user(ctx.author.id, query)
         await func.update_card(card_ids, {"$set": {"owner_id": None, "tag": None, "frame": None, "last_trade_time": 0}})
         upgraded_stars = upgrade_card.stars + len(converted_cards)
