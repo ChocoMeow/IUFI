c258bf1b1b536a0e332d49cec1738f240eec6cf3
j-audrick <janaudricklazaro@gmail.com>
2026-05-30 23:31:14 +0800

    feat: add Card Archive button to HelpView for improved user navigation

--- views/help.py
+++ views/help.py
@@ -16,6 +16,7 @@ class HelpView(discord.ui.View):
         self.add_item(discord.ui.Button(label='Github', emoji=':github:1098265017268322406', url='https://github.com/ChocoMeow/IUFI'))
         self.add_item(discord.ui.Button(label='Beginner Guide', emoji='📗', url='https://docs.google.com/document/d/1VAD20wZQ56S_wDeMJlwIKn_jImIPuxh2lgy1fn17z0c/edit'))
         self.add_item(discord.ui.Button(label='Donate', emoji='🎁', url='https://ko-fi.com/chocoo'))
+        self.add_item(discord.ui.Button(label='Card Archive', emoji='🗂️', url='https://d1w5gpixxno4sl.cloudfront.net'))
         
     async def on_error(self, error, item, interaction) -> None:
         return
